9e0bbc47f11194c1f90d83ab0ac2eaab534f49b0
GHOST <ghostdevbusiness@gmail.com>
2025-02-21 15:03:49 +0000

    chore: add build hook to setup extension

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,8 @@
+import { existsSync } from 'node:fs';
 import { defineConfig } from 'tsup';
+import { isAbsolute, join, resolve } from 'node:path';
+import { cp, rm } from 'node:fs/promises';
+import { env } from 'node:process';
 
 export default defineConfig({
 	entry: ['src/extension.ts'],
@@ -6,4 +10,35 @@ export default defineConfig({
 	dts: false,
 	minify: false,
 	clean: true,
+	async onSuccess() {
+		const BUILD_PATH = join(import.meta.dirname, './dist');
+
+		await cp(
+			join(import.meta.dirname, './metadata.json'),
+			join(BUILD_PATH, './metadata.json'),
+		);
+
+		await cp(
+			join(import.meta.dirname, './LICENSE'),
+			join(BUILD_PATH, './LICENSE'),
+		);
+
+		const HOME = env.HOME;
+
+		if (!HOME || !isAbsolute(HOME)) {
+			console.log('HOME environment variable not found, exiting early');
+			return;
+		}
+
+		const DEST_PATH = join(
+			HOME,
+			'/.local/share/gnome-shell/extensions/fw-fan-ctrl@willow.sh',
+		);
+
+		if (existsSync(DEST_PATH)) {
+			await rm(DEST_PATH, { recursive: true });
+		}
+
+		await cp(BUILD_PATH, DEST_PATH, { recursive: true });
+	},
 });
